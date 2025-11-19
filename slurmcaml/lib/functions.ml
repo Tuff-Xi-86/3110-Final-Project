f6d9f830eda7ac5a9429b1929380dbc7c12f92fd
Mukund <mg2476@cornell.edu>
2025-11-18 23:49:32 -0500

    add skeleton integer matrix

--- slurmcaml/lib/functions.ml
+++ slurmcaml/lib/functions.ml
@@ -1,14 +1,19 @@
 (* each module we make will be an instance of IntegerMatrix signature from
    matrices.ml *)
-open Slurmcaml.Matrices
-
 module type MatrixOperations = sig
   type t
 
   val add : t array array -> t array array -> t array array
   val subtract : t array array -> t array array -> t array array
   val multiply : t array array -> t array array -> t array array
-  val scale : t -> 'a array array -> t array array
+  val scale : t -> t array array -> t array array
 end
 
-module IntegerMatrixOperations : MatrixOperations with type t = int = struct end
+module IntegerMatrixOperations : MatrixOperations with type t = int = struct
+  type t = int
+
+  let add mat1 mat2 = mat1
+  let subtract mat1 mat2 = mat1
+  let multiply mat1 mat2 = mat1
+  let scale k mat1 = mat1
+end
